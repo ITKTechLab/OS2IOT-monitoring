8399fa3deaeb131580dd32a55566b93b4e2011fc
Kristian Risager Larsen <github@kezze.dk>
2024-12-20 14:46:59 +0100

    Support for OS2IOT 1.8.0 + fixed lastseen-bug

--- gateways.py
+++ gateways.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 with open(os.path.dirname(os.path.realpath(__file__)) + "/settings.json") as json_settings_file:
     json_settings = json.load(json_settings_file)
@@ -16,13 +16,32 @@ responsejson = json.loads(response.text)
 
 for item in responsejson['resultList']:
   
-  if not "offline" in item['name'].casefold():
+  if(item['gatewayId'] == "7276ff0039070404"):
+    print("Hold øje")
+
+  if ("silenced_until" in item['tags']):
+    silenced_until = datetime.strptime(item['tags']['silenced_until'], "%d-%m-%yT%H:%M:%S")
+  else:
+    silenced_until = datetime.now() - timedelta(days=1)
+  
+  if (item['status'] == "IN-OPERATION") or (item['status'] == "PROJECT"):
+    print(item['name'] + " - " + item['status'])
     timenotseen = datetime.now().astimezone() - datetime.fromisoformat(item['lastSeenAt'].replace('Z', '+00:00'))
-    if timenotseen.seconds > 3600:
+    if ((timenotseen.days * 86400 + timenotseen.seconds) > 3600) and (silenced_until < datetime.now()):
       textmessage += item['name'] + " - " + item['description'] + ":\r\nLast seen on " + item['lastSeenAt'][0:16] + "\r\n\r\n"
+  elif(item['status'] == "OTHER") or (item['status'] == "PROTOTYPE") or (item['status'] == "NONE"):
+     #print("- " + item['gatewayId'] + " - " + item['status'])
+     pass
+  elif item['status'] is None:
+    #print (item['gatewayId'] + " - error in metadata\r\n\r\n")
+    textmessage += item['gatewayId'] + " - error in metadata\r\n\r\n"
+  else:
+    print("- " + item['gatewayId'] + " - " + item['status'] + " ----- something is wrong")
+    textmessage += item['gatewayId'] + " - error in metadata\r\n\r\n"
+
 
 if textmessage != "":
   sms2go_response = requests.request("POST", sms2go_url, headers={"Content-Type": "application/json", "Authorization": "Bearer " + json_settings['sms2go']['bearer']}, allow_redirects=True, data="{\"body\":\"" + textmessage.encode(encoding="ASCII",errors="ignore").decode() + "\",\"to\":["+json_settings['sms2go']['recipient']+"]}")
-  print(textmessage)
+  #print(textmessage)
 else:
-  print("All gateways are online")
+  print(":: All gateways are online")
