2883954ad01a31ad257cac5a0aedf9b285e56159
Kristian Risager Larsen <github@kezze.dk>
2025-06-13 23:29:02 +0200

    Fix: gateways with lastSeenAt & max-length for SMS2Go

--- gateways.py
+++ gateways.py
@@ -20,12 +20,15 @@ for item in responsejson['resultList']:
     silenced_until = datetime.strptime(item['tags']['silenced_until'], "%d-%m-%yT%H:%M:%S")
   else:
     silenced_until = datetime.now() - timedelta(days=1)
-  
+
+  if item['lastSeenAt'] == None: item['lastSeenAt'] = "2000-01-01T00:00:00Z"  # Fallback for devices that have never been seen
+
   if (item['status'] == "IN-OPERATION") or (item['status'] == "PROJECT"):
-    print(item['name'] + " - " + item['status'])
+    #print(item['status'] + " - " + item['name'])
     timenotseen = datetime.now().astimezone() - datetime.fromisoformat(item['lastSeenAt'].replace('Z', '+00:00'))
     if ((timenotseen.days * 86400 + timenotseen.seconds) > 3600) and (silenced_until < datetime.now()):
-      textmessage += item['name'] + " - " + item['description'] + ":\r\nLast seen on " + item['lastSeenAt'][0:16] + "\r\n\r\n"
+
+      textmessage += item['name'] + ":\r\nLast seen: " + item['lastSeenAt'][0:16] + "\r\n\r\n"
   elif(item['status'] == "OTHER") or (item['status'] == "PROTOTYPE") or (item['status'] == "NONE"):
      #print("- " + item['gatewayId'] + " - " + item['status'])
      pass
@@ -36,9 +39,27 @@ for item in responsejson['resultList']:
     print("- " + item['gatewayId'] + " - " + item['status'] + " ----- something is wrong")
     textmessage += item['gatewayId'] + " - error in metadata\r\n\r\n"
 
+textmessage = textmessage[0:750]  # Limit to 1600 characters to avoid SMS2GO API errors
 
 if textmessage != "":
-  sms2go_response = requests.request("POST", sms2go_url, headers={"Content-Type": "application/json", "Authorization": "Bearer " + json_settings['sms2go']['bearer']}, allow_redirects=True, data="{\"body\":\"" + textmessage.encode(encoding="ASCII",errors="ignore").decode() + "\",\"to\":["+json_settings['sms2go']['recipient']+"]}")
-  #print(textmessage)
+  sms2go_response = requests.request(
+    "POST",
+      sms2go_url,
+      headers={
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + json_settings['sms2go']['bearer']
+    },
+    allow_redirects=True,
+    data=json.dumps({
+      "body": textmessage.encode(encoding="ASCII", errors="ignore").decode(),
+      "to": [json_settings['sms2go']['recipient']]
+    }
+    )
+  )
+  if sms2go_response.status_code == 200:
+      print(":: SMS sent successfully to " + json_settings['sms2go']['recipient'])
+      print(textmessage)
+  else:
+      print(f":: Failed to send SMS: {sms2go_response.status_code} - {sms2go_response.text}")
 else:
   print(":: All gateways are online")
